b2c55b77ef50f186bc7dc3cff0985ee9cb1ceb58
Rajat Sharma <99070424+Rajat-shamra@users.noreply.github.com>
2025-11-11 11:58:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI/CD Demo v2');
+  res.send('Hello from Jenkins CI/CD "Rajat Ji" Demo v3');
 });
 
 app.listen(3000, () => {
